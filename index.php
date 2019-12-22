9e7a8245d7a451e1166b1c55f69f707c7d9e5660
Koki <sakuraikoki@gmail.com>
2019-12-22 13:19:51 +0900

    commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once "function.php";
+session_start();
+$_SESSION['registed']=false;//confirm.phpから登録完了した後にconfirm.phpに戻れなくするためにfalseにする。
+$_SESSION['delete']=false;//confirm.phpから削除完了した後にconfirm.phpに戻れなくするためにfalseにする。
+$_SESSION['h_error']=true;//
+$_SESSION['w_error']=true;//
+$_SESSION['id_judge']=true;
+
+
+try{
+		makeNewPdo();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+		$st=$pdo->query("SELECT title FROM game");
+		while($games=$st->fetch()){
+		$game_titles[]=$games;
+		}
+	
+}catch(PDOException $e){
+	echo 'Connection failed: ';
+	die($e->getMessage());
+}
+
+
+require "d_index.php"
+?>
