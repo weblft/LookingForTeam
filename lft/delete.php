6a804520de686ff4ff97d999ba79c8eeaf0711aa
Koki <sakuraikoki@gmail.com>
2019-12-22 16:16:21 +0900

    commit

--- lft/delete.php
+++ lft/delete.php
@@ -21,16 +21,19 @@ try{
 		$st->bindValue(1,$_SESSION['confirm_id'],PDO::PARAM_STR);
 		$st->execute();
 		$pass=$st->fetch();
+		echo 'flag1';
 		if(password_verify($passwd,$pass[0])){
 			$st=$pdo->prepare("DELETE FROM registration WHERE showid=?");
 			$st->bindValue(1,$_SESSION['confirm_id'],PDO::PARAM_STR);//bindValueはsqlインジェクション対策
 			$st->execute();
 			$_SESSION['delete']=true;
-			header('Location:confirm.php');
-			exit();
+			echo 'flag2';
+		//	header('Location:confirm.php');
+		//	exit();
 		}
 		else{
 			$_SESSION['passflag']=false;
+			echo 'flag3';
 		}
 	}
 
@@ -39,5 +42,5 @@ try{
 	echo 'Connection failed: ';
 	die($e->getMessage());
 }
-require "d_delete.php"
+//require "d_delete.php"
 ?>
