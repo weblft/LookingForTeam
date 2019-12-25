ea41bf76eef6bf8f3df76ce0c6a8418cd20fa91c
Koki <sakuraikoki@gmail.com>
2019-12-25 17:17:31 +0900

    commit

--- lft/index.php
+++ lft/index.php
@@ -8,6 +8,6 @@ $_SESSION['w_error']=true;//
 $_SESSION['id_judge']=true;
 $logo= exif_read_data('logo.png');
 $upper_img= exif_read_data('upper_select.png');
-$lower_img= exif_read_data('lower_select.png')
+$lower_img= exif_read_data('lower_select.png');
 require 'd_index.php';
 ?>
