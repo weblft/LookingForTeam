3a51adcdcb12fe1f67669b213a187065a5c12d68
Koki <sakuraikoki@gmail.com>
2019-12-25 17:15:30 +0900

    commit

--- lft/index.php
+++ lft/index.php
@@ -6,7 +6,8 @@ $_SESSION['delete']=false;//confirm.phpから削除完了した後にconfirm.php
 $_SESSION['h_error']=true;//
 $_SESSION['w_error']=true;//
 $_SESSION['id_judge']=true;
-
-
+$logo= exif_read_data('logo.png');
+$upper_img= exif_read_data('upper_select.png');
+$lower_img= exif_read_data('lower_select.png')
 require 'd_index.php';
 ?>
