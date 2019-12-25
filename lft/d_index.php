d14fb04e7c39437ca4f5ac2817437355f7f524b4
Koki <sakuraikoki@gmail.com>
2019-12-25 16:52:29 +0900

    commit

--- lft/d_index.php
+++ lft/d_index.php
@@ -20,7 +20,7 @@
 		<div class='wrapper'>
 			<div class='main_body'>
 				<div class='main_title'>
-					<img src ='logo.png'>
+					<img src ='logo.png' alt='Looking For Team'>
 				</div>
 				<h1>チームを探すか登録するかを選んでください</h1>
 				<div class='main_select'>
