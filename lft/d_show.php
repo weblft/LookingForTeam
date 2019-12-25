141b599bc9bfb60313c49d7b5c64f3bcafb4b047
Koki <sakuraikoki@gmail.com>
2019-12-25 19:09:01 +0900

    commit

--- lft/d_show.php
+++ lft/d_show.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
+		<meta charset="utf-8"  name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=yes" />
 		<title>Looking For Team</title>
 		<link rel="stylesheet" href="lft.css">
 	</head>
