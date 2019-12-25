98f6827f0011096311a43ee530e7face5985df54
Koki <sakuraikoki@gmail.com>
2019-12-25 17:40:16 +0900

    commit

--- lft/d_index.php
+++ lft/d_index.php
@@ -20,18 +20,22 @@
 		<div class='wrapper'>
 			<div class='main_body'>
 				<div class='main_title'>
-					<img src = <?php echo $logo ?> alt='Looking For Team'>
+					<?php echo	'<img src ='.$logo.'alt="Looking For Team">'; ?>
 				</div>
 				<h1>チームを探すか登録するかを選んでください</h1>
 				<div class='main_select'>
 					<div class='upper_select'>	
 						<div class='box'>
-							<a href='search.php'><img src =<?php echo $upper_img ?>></a>
+							<a href='search.php'>
+								<?php echo <'img src ='.$upper_img.'>'; ?>
+							</a>
 						</div>
 					</div>
 					<div class='lower_select'>
 						<div class='box'>
-							<a href='regist.php'><img src =<?php echo $lower_img?>></a>
+							<a href='regist.php'>
+								<?php echo '<img src ='.$lower_img.'>';?>
+							</a>
 						</div>
 					</div>
 				</div>
