2bdd1c2cda7e3bf2a345a3446b32b7c59275c3bd
Koki <sakuraikoki@gmail.com>
2019-12-29 17:23:05 +0900

    commit

--- lft/d_index.php
+++ lft/d_index.php
@@ -21,9 +21,9 @@
 			<div class="main_body">
 				<div class="main_title">
 					<?php 
-					header("Content-Type: {$result['ContentType']}");
-					echo $result['Body'];
-					header("Content-type: text/html; charset=utf-8");
+						header("Content-Type: {$result['ContentType']}");
+						echo $result['Body'];
+					//header("Content-type: text/html; charset=utf-8");
 					?>
 				</div>
 				<h1>チームを探すか登録するかを選んでください</h1>
