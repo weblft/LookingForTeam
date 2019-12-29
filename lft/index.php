0b1760e7055e8a02a2151f75f1fb67263b0c15a9
Koki <sakuraikoki@gmail.com>
2019-12-29 16:55:25 +0900

    commit

--- lft/index.php
+++ lft/index.php
@@ -7,30 +7,6 @@ $_SESSION['delete']=false;//confirm.phpから削除完了した後にconfirm.php
 $_SESSION['h_error']=true;//
 $_SESSION['w_error']=true;//
 $_SESSION['id_judge']=true;
-$params = [
-'Bucket' => $bucket_name,
-'Key' => 'logo.png',
-];
-
-try{
-	$result = $s3->getObject($params);
-	header("Content-Type:image/png");
-
-	/*$len = $result['ContentLength'];
-	
-	//ファイルを表示
-	header("Content-Type: {$result['ContentType']}");
-	echo $result['Body'];
-	
-	//ファイルダウンロード
-	header('Content-Type: application/force-download;');
-	header('Content-Length: '.$len);
-	header('Content-Disposition: attachment; filename="sample.jpg"');
-	echo $result['Body'];
-	*/
-}catch(S3Exception $e){
-	var_dump($e -> getMessage());
-}   
 
 require 'd_index.php';
 
