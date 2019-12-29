93d9484547aaa484987477a02a4a40d9bdd24113
Koki <sakuraikoki@gmail.com>
2019-12-29 16:10:25 +0900

    commit

--- lft/d_index.php
+++ lft/d_index.php
@@ -20,7 +20,7 @@
 		<div class="wrapper">
 			<div class="main_body">
 				<div class="main_title">
-					<?php echo  $result['body']; ? >
+					<?php echo  $result['Body']; ? >
 				</div>
 				<h1>チームを探すか登録するかを選んでください</h1>
 				<div class="main_select">
