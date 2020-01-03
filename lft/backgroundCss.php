b3d80bb2c9d777dd6855b8f68cafc5fa60e68054
Koki <sakuraikoki@gmail.com>
2020-01-03 19:03:20 +0900

    commit

--- lft/backgroundCss.php
+++ lft/backgroundCss.php
@@ -3,7 +3,7 @@ header('Content-Type:text/css; charset=utf-8');
 session_start(); 
 ?>
 	html , body{
-		background-image:url(<?php echo $_SESSION['$background']; ?>);
+		background-image:url(<?php echo $_SESSION['background']; ?>);
 		background-size:cover;
 		background-attachment: fixed;
 		background-position: center center;<!--常に画像の中心を基準にして見えるように配置-->
